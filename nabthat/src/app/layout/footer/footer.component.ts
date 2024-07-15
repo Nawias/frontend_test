3a5167d6ce953652ec62fb3667306b6db80069b2
Nawias <bronybracket@gmail.com>
2024-07-15 10:17:27 +0200

    Podstawowa funkcjonalność stopki

--- nabthat/src/app/layout/footer/footer.component.ts
+++ nabthat/src/app/layout/footer/footer.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  menuVisible: boolean = false;
+  showMenu(arg: any) {
+    console.log(arg);
+    this.menuVisible = !this.menuVisible;
+  }
 }
